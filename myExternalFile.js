9962110f7b5b60f815e941cf7d0122b3415b8579
SirChristianWilliam <100000473+SirChristianWilliam@users.noreply.github.com>
2022-02-22 11:52:22 -0600

    Create myExternalFile.js

---
+++ myExternalFile.js
@@ -0,0 +1,22 @@
+  
+   function myFunction() {
+   document.getElementById("stickmanColor").style.background = "lightgrey";
+     document.getElementById("covercolor1").style.background = "lightgrey";
+          document.getElementById("covercolor2").style.background = "lightgrey";
+      document.getElementById("covercolor1").style.borderColor = "lightgrey";
+          document.getElementById("covercolor2").style.borderColor = "lightgrey";
+         document.getElementById("covercolor1").style.outlineColor = "lightgrey";
+          document.getElementById("covercolor2").style.outlineColor = "lightgrey";
+
+ }
+   function myBunction() {
+     document.getElementById("stickmanColor").style.background ="grey";
+     document.getElementById("covercolor1").style.background = "grey";
+          document.getElementById("covercolor2").style.background = "grey";
+        document.getElementById("covercolor1").style.borderColor = "grey";
+          document.getElementById("covercolor2").style.borderColor = "grey";
+         document.getElementById("covercolor1").style.outlineColor = "grey";
+          document.getElementById("covercolor2").style.outlineColor = "grey";
+   }
+
+ 
