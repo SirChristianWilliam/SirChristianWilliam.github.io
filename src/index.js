d9609d28a233a9632f805648b6af8f08c365316e
Chris Maki <c.william.developer@gmail.com>
2024-02-27 01:07:30 -0600

    Changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./components/App/App"
+// import App from "./import App from "./App";/App/App"
+import App from "./App";
 
 ReactDOM.render(
     <App />,
